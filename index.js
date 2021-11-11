2cf55f85bd9772636bbf4cf61722db8210717a57
DaEagle01 <fbinkarim01@gmail.com>
2021-11-12 02:18:05 +0600

    60%done

--- index.js
+++ index.js
@@ -20,22 +20,127 @@ async function run() {
     await client.connect();
     const database = client.db("bicycleHub");
     const productCollection = database.collection("products");
+    const orderCollection = database.collection("orders");
     const reviewCollection = database.collection("reviews");
+    const userCollection = database.collection("users");
 
-    // app.get
+    // app.get to get all products
     app.get("/products", async (req, res) => {
       const result = await productCollection.find({}).toArray();
       res.json(result);
     });
 
-    // app.get
+    // app.get to get single bike data
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.json(result);
+    });
+
+    // app.get to get all the reviews
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find({}).toArray();
       res.json(result);
     });
-    // app.put
 
-    // app.post
+    // app.post to order bike
+    app.post("/products/:id", async (req, res) => {
+      const result = await orderCollection.insertOne(req.body);
+      res.json(result);
+    });
+
+    // add users to the database
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.json(result);
+      console.log(result);
+    });
+
+    // check if the use have already been added to the database or not and if added, then do nothing.
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+      console.log(result);
+    });
+
+    // make a user admin by giving it admin role
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      console.log(user.user);
+      const filter = { email: user.user };
+      const options = { upsert: true };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+      console.log(result);
+    });
+
+    // get all the users
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find({}).toArray();
+      res.json(result);
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email, "lagse vai");
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    // get all the orders
+    app.get("/orders", async (req, res) => {
+      const result = await orderCollection.find({}).toArray();
+      res.json(result);
+    });
+
+    // get a single order
+    app.get("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await allBookingCollection.findOne(query);
+      res.json(result);
+    });
+
+    // change status of an order
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateStatus = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: updateStatus.status,
+        },
+      };
+      const result = await orderCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+      console.log(result);
+    });
+
+    // delete an order
+    app.delete("/orders/:_id", async (req, res) => {
+      const id = req.params._id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
